b6b5309be8cabb5748da991a9d9bc2ba3f42b46b
Travis Gorman <thefarbetween@me.com>
2017-03-02 07:50:48 -0600

    clue updates onclick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import categories from './categories.js'
 
@@ -9,30 +9,35 @@ const App = React.createClass({
       categories,
       score: 0,
       showMod: false,
+      clue: {},
     }
   },
-  showModal() {
+  setClue(newClue) {
+    this.setState({clue: newClue})
     this.setState({showMod: true})
   },
+  componentDidUpdate(){
+    console.log('this.state.clue:', this.state.clue )
+  },
   render(){
-    if (this.state.showMod){
-      return (  
-        // <Modal />
-          // value={this.props.value} 
-          // question={this.props.question} 
-          // answer={this.props.answer} />   
+    // if (this.state.showMod){
+    //   return (  
+    //     // <Modal />
+    //       // value={this.props.value} 
+    //       // question={this.props.question} 
+    //       // answer={this.props.answer} />   
 
-       <div className="App">
-         <Header tagline={"This is a modal"}/>
-         <Board categories={this.state.categories} showModal={this.showModal} />
-         <Score score={this.state.score} />
-        </div>
-      )
-    }
+    //    <div className="App">
+    //      <Header tagline={this.state.tagline}/>
+    //      <Board categories={this.state.categories} setClue={this.setClue} />
+    //      <Score score={this.state.score} />
+    //     </div>
+    //   )
+    // }
     return (  
       <div className="App">
-        <Header tagline={"We give you answers, and you ask us questions"}/>
-        <Board categories={this.state.categories} showModal={this.showModal}/>
+        <Header tagline={this.state.clue.question}/>
+        <Board categories={this.state.categories} setClue={this.setClue}/>
         <Score score={this.state.score} />
       </div>
     )
@@ -57,34 +62,38 @@ const Board = React.createClass({
       .map((category, i)=>{
         let title = category.title
         let clues = category.clues
-        return <Category title={title} clues={clues} key={i} showModal={this.props.showModal}/>
+        return (  
+                <Category 
+                  title={title} 
+                  clues={clues} 
+                  key={i} 
+                  setClue={this.props.setClue}/>
+        )
       })
     return (
-      <div className="Board">
-        {board}
-      </div>
+            <div className="Board">
+              {board}
+            </div>
     )
   }
 })
 
 const Category = React.createClass({
-  clickHandler(){
+  // clickHandler(){
 
-  },
+  // },
   render() {
     let clues = this.props.clues
       .map((clue, i)=>{
         return (  
-          <Clue
-            onClick={this.clickHandler}
-            value={clue.value} 
-            question={clue.question} 
-            answer={clue.answer}
-            showModal={this.props.showModal}
-            key={i} />
+                <Clue
+                  value={clue.value}
+                  question={clue.question}
+                  answer={clue.answer}
+                  setClue={this.props.setClue}
+                  key={i}/>
         )
       })
-
     return (
       <div className="Category">
         <section className="category-title">
@@ -97,10 +106,12 @@ const Category = React.createClass({
 })
 
 const Clue = React.createClass({
-
+  clickHandler() {
+    this.props.setClue(this.props)
+  },
   render() {
     return (
-      <article onClick={this.props.showModal}>
+      <article onClick={this.clickHandler}>
         <h1 className="value">{this.props.value}</h1>
       </article>
     )
